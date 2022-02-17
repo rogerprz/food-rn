27d65680dd2428fb735b9fc4e9fbecb32f5039f3
Roger Perez <ropere@microsoft.com>
2022-02-17 10:01:52 -0800

    Add nav + search bar + search screen

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const SearchScreen = () => {
+    return (<View>
+      <Text>Search Screen</Text>
+    </View>
+    );
+}
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
