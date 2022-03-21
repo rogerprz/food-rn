860632fa39bfccef653675a34b5e1a7d3ee0b0a0
Roger Perez <ropere@microsoft.com>
2022-03-21 14:54:04 -0700

    Add details to post

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,26 +1,31 @@
 import { FlatList, StyleSheet, Text, View } from 'react-native';
+import ResultsDetail from './ResultsDetail';
 
-const ResultsList = ({title, results}) => {
-    return (
-    <View>
-      <Text style={styles.backgroundStyle}>{title}</Text>
-      <Text style={styles.backgroundStyle}>{results.length}</Text>
+const ResultsList = ({ title, results }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
       <FlatList
         horizontal
         data={results}
-        keyExtractor={result => result.id}
-        renderItem={({item}) => {
-          return (<Text>{item.name}</Text>);
+        keyExtractor={(result) => result.id}
+        renderItem={({ item }) => {
+          return <ResultsDetail result={item} />;
         }}
-        />
+      />
     </View>
-    );
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  backgroundStyle: {
-    fontSize: 20,
+  title: {
+    fontSize: 18,
     fontWeight: 'bold',
+    marginLeft: 15,
+    marginBottom: 5
+  },
+  container: {
+    marginBottom: 10
   }
 });
 
